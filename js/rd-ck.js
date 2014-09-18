9491b8fd262c4b89411562939dd6ac6b6461757b
rdanieldesign <rdanieldesign@gmail.com>
2014-09-17 23:26:27 -0400

    General fixes and file restructuring

--- js/rd-ck.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$("#hamburger").click(function(){$(this).toggleClass("active");$("nav ul").toggleClass("active")})});
--- js/rd.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$("#hamburger").click(function(){
-		$(this).toggleClass('active');
-		$('nav ul').toggleClass('active');
-	});
-});
